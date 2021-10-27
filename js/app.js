817866b9be389d54ee22da4bcea1868fb154aee2
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-10-27 08:20:16 -0600

    my first hash table

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
-//document.body.style.backgroundColor = "red";
-//test me
+class MyHashMap {
+	constructor() {}
+	put(key, value) {
+		this[key] = value;
+	}
+	get(key) {
+		return this[key] || -1;
+	}
+	remove(key) {
+		delete this[key];
+	}
+}
+
+const myHashMap = new MyHashMap();
+
+myHashMap.put('name', 'Preston');
+myHashMap.put('name', 'Larry');
+console.log(myHashMap.get('namee'));
+console.log(myHashMap);
